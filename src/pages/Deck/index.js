4bf3d1d47058c6a8c3d6e20f8f0a19a84f3f5068
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2022-09-09 16:00:20 -0300

    css and minor changes on deck edit

--- src/pages/Deck/index.js
+++ src/pages/Deck/index.js
@@ -1,17 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import React, { useEffect, useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import { FaTrash, FaEdit } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaReply, FaEye } from 'react-icons/fa';
+import { get, isEmpty } from 'lodash';
+import PropTypes from 'prop-types';
+import { toast } from 'react-toastify';
 
-import { Container } from '../../styles/GlobalStyles';
-import {} from './styled';
-// import Loading from '../../components/Loading';
-// import axios from '../../services/axios';
+// import { Container } from '../../styles/GlobalStyles';
+import {
+  LocalContainer,
+  CardShowStyle,
+  CardShowContainer,
+  DeckNameEditStyle,
+  CardListStyle,
+  CardStyle,
+} from './styled';
+import Loading from '../../components/Loading';
+import axios from '../../services/axios';
 
-export default function Deck() {
+export default function Deck({ match }) {
+  const id = get(match, 'params.id', '');
+  const [deck, setDeck] = useState([]);
+  const [name, setName] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // const [cards, setCards] = useState([]);
+
+  const [cardShow, setCardShow] = useState({});
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function getDeck() {
+      // fazer try catch
+      setIsLoading(true);
+      const { data } = await axios.get(`/decks/${id}`);
+      console.log('data ', data);
+      setDeck(data);
+      setName(data.name);
+      setIsLoading(false);
+    }
+
+    getDeck();
+  }, [id]);
+
+  const handleEditDeckName = async () => {
+    if (name.length < 1) return;
+    if (!window.confirm(`Do you want to rename this deck as '${name}?'`))
+      return;
+
+    try {
+      setIsLoading(true);
+      await axios.put(`/decks/${id}`, { name });
+      setIsLoading(false);
+      toast.success('Deck has been renamed successfully!');
+    } catch (err) {
+      setIsLoading(false);
+      // const status = get(err, 'response.status', 0);
+      const data = get(err, 'response.data', {});
+      const errors = get(data, 'errors', []);
+
+      if (errors.length > 0) {
+        errors.map((error) => toast.error(error));
+      } else {
+        toast.error('Erro desconhecido');
+      }
+    }
+  };
+
+  const handleShowCard = (e, card, index) => {
+    console.log('card info: ', card);
+    console.log('index info: ', index);
+    setCardShow(card);
+  };
+
   return (
-    <Container>
-      <h1>Olá deck</h1>
-    </Container>
+    <>
+      <LocalContainer>
+        <Loading isLoading={isLoading} />
+        <h1>Edit your deck here</h1>
+        {deck ? (
+          <DeckNameEditStyle>
+            <label htmlFor="name">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Edit Deck Name"
+              />
+            </label>
+            <button type="button" onClick={handleEditDeckName}>
+              <FaReply size={16} />
+            </button>
+          </DeckNameEditStyle>
+        ) : null}
+
+        <CardListStyle>
+          {deck && deck.Cards
+            ? deck.Cards.map((card, index) => (
+                <div key={card.id}>
+                  <CardStyle>
+                    <span>{card.front}</span>
+                    <FaEye onClick={(e) => handleShowCard(e, card, index)} />
+                  </CardStyle>
+                </div>
+              ))
+            : null}
+        </CardListStyle>
+      </LocalContainer>
+      <CardShowContainer>
+        <h1>Card Info</h1>
+        {!isEmpty(cardShow) ? (
+          <div>
+            <CardShowStyle>
+              <p>Front</p>
+              <div>{cardShow.front}</div>
+            </CardShowStyle>
+            <CardShowStyle>
+              <p>Back</p>
+              <div>{cardShow.back}</div>
+            </CardShowStyle>
+            <Link to={`/card/${cardShow.id}/edit`}>
+              <button type="button">Edit Card</button>
+            </Link>
+          </div>
+        ) : null}
+      </CardShowContainer>
+      {/* <Container>fsdkajfh</Container> */}
+    </>
   );
 }
+
+Deck.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Deck/styled.js
+++ src/pages/Deck/styled.js
@@ -1,5 +1,133 @@
 import styled from 'styled-components';
+import { primaryColor, primaryDarkColor } from '../../config/colors';
 
 export const Paragrafo = styled.p``;
 
 export const DeckBackgroud = styled.div``;
+
+export const LocalContainer = styled.section`
+  max-width: 360px;
+  max-height: 70vh;
+  background-color: #fff;
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: ${primaryColor};
+    text-align: center;
+    margin-bottom: 15px;
+    /* text-transform: capitalize; */
+  }
+
+  overflow-y: auto;
+`;
+
+export const DeckNameEditStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  label {
+    width: 100%;
+    flex-grow: 1;
+  }
+
+  input {
+    padding: 10px;
+    width: 100%;
+    border: 1px solid ${primaryDarkColor};
+    border-radius: 7px;
+  }
+
+  button {
+    margin-left: 5px;
+    flex-shrink: 1;
+  }
+`;
+
+export const CardListStyle = styled.div`
+  margin: 20px 0 0 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardStyle = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 5px;
+  background: #efefef;
+  padding: 10px 15px;
+  border-radius: 10px;
+
+  span {
+    margin-right: 20px;
+    flex-grow: 1;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    /* width: 100px; */
+  }
+
+  a {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    cursor: pointer;
+  }
+
+  svg {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    cursor: pointer;
+    /* height: 100%; */
+    flex-shrink: 0;
+    color: ${primaryDarkColor};
+    transition: color 300ms;
+  }
+
+  svg:hover {
+    color: ${primaryColor};
+  }
+
+  svg + a {
+    margin-left: 15px;
+  }
+`;
+
+export const CardShowContainer = styled.div`
+  max-width: 360px;
+  background-color: #fff;
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    margin-bottom: 15px;
+    text-align: center;
+    color: ${primaryColor};
+  }
+`;
+
+export const CardShowStyle = styled.div`
+  margin-bottom: 15px;
+
+  p {
+    text-align: center;
+    background-color: ${primaryColor};
+    color: white;
+    padding: 5px 0 5px 0;
+    border-top-left-radius: 7px;
+    border-top-right-radius: 7px;
+  }
+
+  div {
+    color: white;
+    background-color: ${primaryDarkColor};
+    padding: 10px;
+    border-bottom-left-radius: 7px;
+    border-bottom-right-radius: 7px;
+  }
+`;
