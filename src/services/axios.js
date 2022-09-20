2209c27d0abbade636c8d8c341258f3c26c42026
Guilherme Antunes <guilherme.silva.antunes1999@gmail.com>
2022-09-20 10:59:05 -0300

    env file for axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+import { join } from 'path';
+
+dotenv.config({ path: join(__dirname, '../../.env') });
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  // baseURL: 'http://localhost:3001',
+  baseURL: process.env.BASE_URL,
 });
